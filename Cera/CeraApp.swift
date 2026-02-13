30f2be027519dca41c3316a5e6c69ea46a0b8b88
Oskar Pajka <oskar.pajka16@gmail.com>
2026-02-13 18:56:01 +0100

    Initial Commit

---
+++ Cera/CeraApp.swift
@@ -0,0 +1,17 @@
+//
+//  CeraApp.swift
+//  Cera
+//
+//  Created by Oskar Pajka on 13/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct CeraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Cera/ContentView.swift
@@ -0,0 +1,17 @@
+//
+//  ContentView.swift
+//  Cera
+//
+//  Created by Oskar Pajka on 13/02/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("Cera")
+        }
+        .padding()
+    }
+}
